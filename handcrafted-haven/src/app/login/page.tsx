f42dae565d402f5d3a1e3bc1fdafc8331d70bb85
dagwan1 <dan21017@byui.edu>
2024-10-28 11:06:47 +0100

    fixt deployment error in login page

--- handcrafted-haven/src/app/login/page.tsx
+++ handcrafted-haven/src/app/login/page.tsx
@@ -6,6 +6,11 @@ import RootLayout from '../layout';
 import styles from '../../styles/Login.module.css';
 import { AiOutlineUser, AiOutlineLock, AiOutlineMail, AiOutlineKey } from 'react-icons/ai';
 
+// Define types for errors
+interface ErrorResponse {
+    message: string;
+}
+
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -66,7 +71,7 @@ const Login = () => {
 
     const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (!validateLogin()) return; 
+        if (!validateLogin()) return;
 
         try {
             const response = await fetch('https://handcrafted-haven-api.onrender.com/users/login', {
@@ -78,19 +83,21 @@ const Login = () => {
             });
 
             if (!response.ok) {
-                const data = await response.json();
+                const data: ErrorResponse = await response.json();
                 throw new Error(data.message || 'Login failed');
             }
 
             setError('');
             window.location.href = '/sellers/dashboard';
-        } catch (err: any) {
-            setError(err.message);
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            }
         }
     };
 
     const handleForgotPassword = async () => {
-        if (!validateForgotPassword()) return; 
+        if (!validateForgotPassword()) return;
 
         try {
             const response = await fetch('https://handcrafted-haven-api.onrender.com/users/forgot-password', {
@@ -102,20 +109,22 @@ const Login = () => {
             });
 
             if (!response.ok) {
-                const data = await response.json();
+                const data: ErrorResponse = await response.json();
                 throw new Error(data.message || 'Failed to send reset password email');
             }
 
             setResetMessage(`Password reset link sent to: ${forgotPasswordEmail}`);
             setShowForgotPassword(false);
             setShowResetForm(true);
-        } catch (err: any) {
-            setResetMessage(err.message);
+        } catch (err) {
+            if (err instanceof Error) {
+                setResetMessage(err.message);
+            }
         }
     };
 
     const handleResetPassword = async () => {
-        if (!validateResetPassword()) return; 
+        if (!validateResetPassword()) return;
 
         try {
             const response = await fetch(`https://handcrafted-haven-api.onrender.com/users/reset-password/${token}`, {
@@ -127,7 +136,7 @@ const Login = () => {
             });
 
             if (!response.ok) {
-                const data = await response.json();
+                const data: ErrorResponse = await response.json();
                 throw new Error(data.message || 'Failed to reset password');
             }
 
@@ -143,107 +152,109 @@ const Login = () => {
             setTimeout(() => {
                 router.push('/login');
             }, 3000); // 3 seconds
-        } catch (err: any) {
-            setResetMessage(err.message);
+        } catch (err) {
+            if (err instanceof Error) {
+                setResetMessage(err.message);
+            }
         }
     };
 
     return (
         <RootLayout pageTitle="User Login">
-        <div className={styles.login}>
-            <h1>User Login</h1>
-            <p>Enter your username and password to log into your account.</p>
-            <form onSubmit={handleLogin}>
-                <div className={styles.inputGroup}>
-                    <AiOutlineUser className={styles.icon} />
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className={styles.inputGroup}>
-                    <AiOutlineLock className={styles.icon} />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                {error && <p className={styles.error}>{error}</p>}
-                <button type="submit" className={styles.loginButton}>Login</button>
-                <p onClick={() => setShowForgotPassword(true)} className={styles.forgotPassword}>Forgot Password?</p>
-            </form>
-            <p className={styles.userPrompt}>Don't have an account? <span onClick={() => router.push('/user')} className={styles.userLink}>Create Account</span></p>
-
-            {showForgotPassword && (
-                <div className={styles.popup}>
-                    <div className={styles.popupContent}>
-                        <h2>Reset Password</h2>
-                        <p>Enter your email address to receive a password reset token.</p>
-                        <div className={styles.inputGroup}>
-                            <AiOutlineMail className={styles.icon} />
-                            <input
-                                type="email"
-                                placeholder="Enter your email"
-                                value={forgotPasswordEmail}
-                                onChange={(e) => setForgotPasswordEmail(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <button onClick={handleForgotPassword} className={styles.popupButton}>Send Reset Link</button>
-                        <p className={styles.closePopup} onClick={() => setShowForgotPassword(false)}>Close</p>
-                        {resetMessage && <p className={styles.resetMessage}>{resetMessage}</p>}
+            <div className={styles.login}>
+                <h1>User Login</h1>
+                <p>Enter your username and password to log into your account.</p>
+                <form onSubmit={handleLogin}>
+                    <div className={styles.inputGroup}>
+                        <AiOutlineUser className={styles.icon} />
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
                     </div>
-                </div>
-            )}
-
-            {showResetForm && (
-                <div className={styles.popup}>
-                    <div className={styles.popupContent}>
-                        <h2>Enter Reset Token</h2>
-                        <p>Please enter the reset token and your new password.</p>
-                        <div className={styles.inputGroup}>
-                            <AiOutlineKey className={styles.icon} />
-                            <input
-                                type="text"
-                                placeholder="Reset Token"
-                                value={token}
-                                onChange={(e) => setToken(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className={styles.inputGroup}>
-                            <AiOutlineLock className={styles.icon} />
-                            <input
-                                type="password"
-                                placeholder="New Password"
-                                value={newPassword}
-                                onChange={(e) => setNewPassword(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className={styles.inputGroup}>
-                            <AiOutlineLock className={styles.icon} />
-                            <input
-                                type="password"
-                                placeholder="Confirm Password"
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <button onClick={handleResetPassword} className={styles.popupButton}>Reset Password</button>
-                        <p className={styles.closePopup} onClick={() => setShowResetForm(false)}>Close</p>
-                        {resetMessage && <p className={styles.resetMessage}>{resetMessage}</p>}
+                    <div className={styles.inputGroup}>
+                        <AiOutlineLock className={styles.icon} />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
                     </div>
-                </div>
-            )}
-        </div>
+                    {error && <p className={styles.error}>{error}</p>}
+                    <button type="submit" className={styles.loginButton}>Login</button>
+                    <p onClick={() => setShowForgotPassword(true)} className={styles.forgotPassword}>Forgot Password?</p>
+                </form>
+                <p className={styles.userPrompt}>Don&apos;t have an account? <span onClick={() => router.push('/user')} className={styles.userLink}>Create Account</span></p>
+
+                {showForgotPassword && (
+                    <div className={styles.popup}>
+                        <div className={styles.popupContent}>
+                            <h2>Reset Password</h2>
+                            <p>Enter your email address to receive a password reset token.</p>
+                            <div className={styles.inputGroup}>
+                                <AiOutlineMail className={styles.icon} />
+                                <input
+                                    type="email"
+                                    placeholder="Enter your email"
+                                    value={forgotPasswordEmail}
+                                    onChange={(e) => setForgotPasswordEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <button onClick={handleForgotPassword} className={styles.popupButton}>Send Reset Link</button>
+                            <p className={styles.closePopup} onClick={() => setShowForgotPassword(false)}>Close</p>
+                            {resetMessage && <p className={styles.resetMessage}>{resetMessage}</p>}
+                        </div>
+                    </div>
+                )}
+
+                {showResetForm && (
+                    <div className={styles.popup}>
+                        <div className={styles.popupContent}>
+                            <h2>Enter Reset Token</h2>
+                            <p>Please enter the reset token and your new password.</p>
+                            <div className={styles.inputGroup}>
+                                <AiOutlineKey className={styles.icon} />
+                                <input
+                                    type="text"
+                                    placeholder="Reset Token"
+                                    value={token}
+                                    onChange={(e) => setToken(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.inputGroup}>
+                                <AiOutlineLock className={styles.icon} />
+                                <input
+                                    type="password"
+                                    placeholder="New Password"
+                                    value={newPassword}
+                                    onChange={(e) => setNewPassword(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.inputGroup}>
+                                <AiOutlineLock className={styles.icon} />
+                                <input
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <button onClick={handleResetPassword} className={styles.popupButton}>Reset Password</button>
+                            <p className={styles.closePopup} onClick={() => setShowResetForm(false)}>Close</p>
+                            {resetMessage && <p className={styles.resetMessage}>{resetMessage}</p>}
+                        </div>
+                    </div>
+                )}
+            </div>
         </RootLayout>
     );
 };
